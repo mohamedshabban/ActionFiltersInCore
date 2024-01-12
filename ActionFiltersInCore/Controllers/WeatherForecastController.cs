02041d7e960e34ab8e141baddca71c0aa40ee9f9
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2024-01-12 23:39:51 +0200

    Add project files.

---
+++ ActionFiltersInCore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using ActionFiltersInCore.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionFiltersInCore.Controllers
+{
+    [ApiController]
+    [Route("weather/api")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// This will cause the LogActionFilter to be executed before and after the Index method is called.
+        /// </summary>
+        /// <returns></returns>
+        [LogActionFilter]
+        [HttpGet("Index")]
+        public IActionResult Index() 
+        {
+            return Ok();
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+}
---
+++ ActionFiltersInCore/Utility/LogActionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace ActionFiltersInCore.Utility
+{
+    /// <summary>
+    /// logs the start and end time of a controller action method:
+    /// </summary>
+    public class LogActionFilter: ActionFilterAttribute
+    {
+        /// <inheritdoc />
+        private Stopwatch _stopwatch;
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        /// <inheritdoc />
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _stopwatch.Stop();
+            Console.WriteLine($"The action took {_stopwatch.ElapsedMilliseconds} ms to execute");
+        }
+
+    }
+}
